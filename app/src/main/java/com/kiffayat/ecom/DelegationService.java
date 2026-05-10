9343343f0081cdcbb4044a596014209037e47f17
Danish Mohiudin <danishmohiudin14@gmail.com>
2026-05-10 19:19:37 +0530

    Initial commit of bubblewrap PWA app kj buid mart

---
+++ app/src/main/java/com/kiffayat/ecom/DelegationService.java
@@ -0,0 +1,14 @@
+package com.kiffayat.ecom;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
